a6be120da11df7915722b33b2f9542646217460c
scottykrieg <118317810+scottykrieg@users.noreply.github.com>
2023-05-22 19:26:22 -0400

    Created Pages directory

--- app/components/header.js
+++ app/components/header.js
@@ -13,32 +13,43 @@ const lato = Lato({
 
 export default function Header() {
   return (
-    <div className={courgette.className}>
-      <div className="flex p-2 justify-around ">
-        <h1 className="text-6xl">VaiVerb</h1>{" "}
-        <div className={lato.className}>
-          <input
-            placeholder="Shop for used & new music gear..."
-            className="mx-2 border-2 border-black w-96"
-          ></input>
-        </div>
-        <div className={lato.className}>
-          <div className="flex">
-            <button className="border-2 rounded-full border-black p-1">
-              Sell Your Gear
-            </button>
-            <div className="m-2">
-              <SlHeart />
-              <p>Favorites</p>
-            </div>
-            <div className="m-2">
-              <SlBasket />
-              <p>Cart</p>
-            </div>
-            <p className="m-2">Sign Up</p>
-            <p className="m-2">Log In</p>
+    <div>
+      <div className={courgette.className}>
+        <div className="flex p-2 justify-around ">
+          <h1 className="text-6xl">VaiVerb</h1>{" "}
+          <div className={lato.className}>
+            <input
+              placeholder="Shop for used & new music gear..."
+              className="mx-2 border-2 border-black w-96"
+            ></input>
           </div>
-        </div>{" "}
+          <div className={lato.className}>
+            <div className="flex">
+              <button className="border-2 rounded-full border-black p-1">
+                Sell Your Gear
+              </button>
+              <div className="m-2">
+                <button>
+                  <SlHeart />
+                  <p>Favorites</p>
+                </button>
+              </div>
+              <div className="m-2">
+                {" "}
+                <button>
+                  <SlBasket />
+                  <p>Cart</p>
+                </button>
+              </div>
+              <button>
+                <p className="m-2">Sign Up</p>
+              </button>
+              <button>
+                <p className="m-2">Log In</p>
+              </button>
+            </div>
+          </div>{" "}
+        </div>
       </div>
     </div>
   );
