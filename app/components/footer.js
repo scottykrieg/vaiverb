b35201e983d951e2fbbec0b8adee6c496cb6d22e
scottykrieg <118317810+scottykrieg@users.noreply.github.com>
2023-05-25 18:42:37 -0400

    Updated Footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,16 +1,21 @@
-import { Courgette } from "next/font/google";
+import { Courgette, Lato } from "next/font/google";
 
 const courgette = Courgette({
   weight: "400",
   subsets: ["latin"],
 });
 
+const lato = Lato({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Footer({ Component, pageProps }) {
   return (
-    <div className={courgette.className}>
+    <div className={lato.className}>
       <div className="p-2">
         <h1>VaiVerb Footer</h1>
-        <h6>Copyright 2023</h6>
+        <h6>© 2023 Vaiverb.com LLC</h6>
       </div>{" "}
     </div>
   );
